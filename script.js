27a8bc37eb8bd3c79cd62a1e1e274d6b3779894d
Arigblack <mishragendu007@gmail.com>
2026-01-04 04:57:26 +0530

    Add image slideshow functionality to script.js

--- script.js
+++ script.js
@@ -20,3 +20,14 @@ function openImg(img){
 document.getElementById("modal").onclick = function(){
   this.style.display="none";
 };
+let slides = [
+  "images/1.jpg","images/2.jpg","images/3.jpg",
+  "images/4.jpg","images/5.jpg","images/6.jpg",
+  "images/7.jpg","images/8.jpg","images/9.jpg"
+];
+
+let i = 0;
+setInterval(()=>{
+  i = (i+1) % slides.length;
+  document.getElementById("slideShow").src = slides[i];
+}, 3000);
